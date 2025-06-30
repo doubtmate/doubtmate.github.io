b7ed5503aa4eb9ce8985179a4f9b0130039d045d
doubtmate <doubtmate247@gmail.com>
2025-06-30 07:35:29 +0000

    input field animation

--- script.js
+++ script.js
@@ -4,3 +4,20 @@
   menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
   });
+
+  const typingText = "Type your JEE doubt here...";
+const typingEl = document.getElementById("typing-text");
+
+let index = 0;
+
+function typeEffect() {
+  if (index < typingText.length) {
+    typingEl.textContent += typingText.charAt(index);
+    index++;
+    setTimeout(typeEffect, 100);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  typeEffect();
+});
